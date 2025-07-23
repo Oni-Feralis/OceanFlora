b324f334f4d1cedc6b9e38aa80af5549d6995533
Oni_Feralis <reijngay@gmail.com>
2025-07-22 22:58:44 -0400

    Seatato added

--- src/main/java/net/oni_feralis/oceanflora/OceanFlora.java
+++ src/main/java/net/oni_feralis/oceanflora/OceanFlora.java
@@ -1,5 +1,8 @@
 package net.oni_feralis.oceanflora;
 
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.oni_feralis.oceanflora.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -31,6 +34,8 @@ public class OceanFlora {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -44,7 +49,9 @@ public class OceanFlora {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.SEATATO);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/oni_feralis/oceanflora/item/ModItems.java
@@ -0,0 +1,18 @@
+package net.oni_feralis.oceanflora.item;
+
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.oni_feralis.oceanflora.OceanFlora;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(OceanFlora.MOD_ID);
+
+    public static final DeferredItem<Item> SEATATO = ITEMS.register("seatato",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
